86ef7095dcbd17deb8b781e7efce32821d56f2fa
sasogu <sasogu@users.noreply.github.com>
2025-09-14 10:53:44 +0000

    deploy: 6de311b5e8c3384a0a752918cb7c3f8b899b5746

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '0.2.9'; // Este valor será reemplazado automáticamente
+const CACHE_VERSION = '0.3.0'; // Este valor será reemplazado automáticamente
 const CACHE_NAME = `linkhub-cache-${CACHE_VERSION}`;
 
 self.addEventListener('install', event => {
